35630878278c856442d39b5802b86b73465ee374
Nishikawa <vi.srnishikawa.2007@gmail.com>
2024-09-30 11:59:22 -0300

    pagina 404 NotFound

---
+++ client/kiarnet/src/pages/NotFound/index.jsx
@@ -0,0 +1,17 @@
+import './index.scss'
+
+export default function Notfound(){
+    return (
+        <div className="mae">
+            
+        <div className="meio">
+    <h1 className='num'>404</h1>
+    <p className='titulo'>Página não encontrada</p>
+    <p className='subt'>O link que você seguiu pode estar quebrado, ou a página pode ter sido removida.</p>
+    <a href="">Voltar ao início</a>
+   
+        </div>
+        <p className='footer'>© 2024 KiarNet. Todos os direitos reservados.</p>
+        </div>
+    )
+}
