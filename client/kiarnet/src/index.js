b70b79a83eace0be804703f1e581bf7d283b379e
Vinicius Carmo <131480329+Running-Agama@users.noreply.github.com>
2024-10-01 13:44:22 +0000

    rotas adicionadas

--- client/kiarnet/src/index.js
+++ client/kiarnet/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import {Route, BrowserRouter,Routes} from 'react-router-dom'
 
 import Notfound from './pages/NotFound';
+import Cadastro from './pages/Cadastro';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-     <Notfound/> 
+     
+     <BrowserRouter>
+          <Routes>
+               <Route path='*' element={<Notfound/>}/>
+               <Route path='/cadastro' element={<Cadastro/>}/>
+          </Routes>
+     </BrowserRouter>
 
 );
--- client/kiarnet/src/pages/Cadastro/index.jsx
+++ client/kiarnet/src/pages/Cadastro/index.jsx
@@ -1,1 +1,12 @@
-//placeholder
+export default function Cadastro(){
+    const [passo, setPasso] = useState(0)
+    
+    return(
+
+    <div className="">
+        <p>Barra de progresso deve estar aqui</p>
+
+
+    </div>
+    )
+}
