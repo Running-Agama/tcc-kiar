6509cfaff2ab4b20341a77c97eaf9e5443b49fc8
Vinicius Carmo <131480329+Running-Agama@users.noreply.github.com>
2024-10-01 14:06:20 +0000

    cadastro

--- client/kiarnet/src/pages/Cadastro/index.jsx
+++ client/kiarnet/src/pages/Cadastro/index.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import InformeDados from '../../components/cadastro-passos/Informedados';
+import AgendaInstalacao from '../../components/cadastro-passos/Agendainstalacao';
 export default function Cadastro(){
     const [passo, setPasso] = useState(0)
 
+    function verificarPasso(){
+        if(passo === 1){
+            return <InformeDados/>
+        }
+        if(passo === 2){
+            return <AgendaInstalacao/>
+        }
+    }
     return(
 
     <div className="">
         <p>Barra de progresso deve estar aqui</p>
+        
+        {verificarPasso}
 
-
+        <button onClick={setPasso(passo + 1)}>Proximo passo</button>
     </div>
     )
 }
