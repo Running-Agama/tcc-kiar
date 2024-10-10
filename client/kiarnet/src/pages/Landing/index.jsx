2dcd634daa671d6d4a3e37388bc05461dc3df069
Vinicius Carmo <131480329+Running-Agama@users.noreply.github.com>
2024-10-10 10:52:13 +0000

    classnameconsertado

--- client/kiarnet/src/pages/Landing/index.jsx
+++ client/kiarnet/src/pages/Landing/index.jsx
@@ -16,23 +16,23 @@ export default function Landing() {
             </nav>
 
 
-            <section class="hero-section">
-                <div class="hero-content">
+            <section className="hero-section">
+                <div className="hero-content">
                     <h1>O MELHOR PROVEDOR DE INTERNET PARA SUA CASA</h1>
                         <p>Experimente a diferença! Temos planos flexíveis e atendimento personalizado. Entre em contato e transforme sua navegação!</p>
                             <div class="buttons">
-                                <button class="btn-primary">Começar</button>
-                                <button class="btn-secondary">Ver mais</button>
+                                <button className="btn-primary">Começar</button>
+                                <button className="btn-secondary">Ver mais</button>
                             </div>
                         </div>
                         </section>
 
-                            <section class="info-section">
-                                <div class="info-box">
+                            <section className="info-section">
+                                <div className="info-box">
                                     <h2>Descubra o melhor da conexão</h2>
                                     <p>Diga adeus à lentidão e frustrações! É hora de explorar planos de internet feitos para acompanhar todas as suas necessidades, seja para navegar, trabalhar ou aproveitar o melhor do entretenimento.</p>
                                 </div>
-                                <div class="info-box">
+                                <div className="info-box">
                                     <h2>Encontre o plano perfeito para você</h2>
                                     
                                     <p>Escolha a velocidade ideal: 100 Mbps para navegação e streaming fluidos, 200 Mbps para tarefas que exigem mais dados, ou até mesmo planos de 300 Mbps para downloads ultrarrápidos e desempenho sem igual.</p><p>Escolha a velocidade ideal: 100 Mbps para navegação e streaming fluidos, 200 Mbps para tarefas que exigem mais dados, ou até mesmo planos de 300 Mbps para downloads ultrarrápidos e desempenho sem igual.</p>
