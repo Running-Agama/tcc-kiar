328b6f60991815afd7f7b789cf6cf4baeddbf93d
running-agama <carmov106@proton.me>
2024-10-16 22:54:19 -0300

    area de cadastro reescrita e melhorada com formulario decente, falta ajustes e contato com a api

--- client/kiarnet/src/components/cadastro-passos/Informedados/index.jsx
+++ client/kiarnet/src/components/cadastro-passos/Informedados/index.jsx
@@ -6,59 +6,13 @@ import { useForm } from 'react-hook-form';
 
 export default function InformeDados() {
 
-    const [
-        cep, setCep,
-        nomeCompleto, setNomeCompleto, 
-        celular, setCelular,
-        telefone, setTelefone,
-        email, setEmail,
-        cpf, setCpf,
-        dtNascimento, setDtNascimento,
-        numeroEndereco, setNumeroEndereco
-
-        ] 
-    = useContext(contextoCadastro)
+   const {}
 
     const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
     console.log(errors);
 
     return (
-        <div className="conteudo">
-            <h1>Informe seus dados</h1>
-            <div className="campos-informedados">
-            <form onSubmit={handleSubmit(onSubmit)}>
 
-                <div className="linha">
-                <input type="text" placeholder="Nome completo" {...register("Nome completo", {required: true})} required className='campo-um-input'  onChange={(e)=>setNomeCompleto(e.target.value)}/>
-                </div>
-                
-                <div className="linha">
-                    <input type="number" placeholder="CEP:" {...register("CEP", {required: true})} className='campo-um-input' onChange={(e)=>setCep(e.target.value)} />
-                </div>
-
-                <div className="linha">
-                    <input type="tel" {...register("celular", {required: true})} className="campo" placeholder="Celular" onChange={(e)=>setCelular(e.target.value)} />
-
-                    <input type="tel" className="campo" placeholder="Telefone (opcional)" onChange={(e)=>setTelefone(e.target.value)} />
-                </div>
-
-                
-                <div className="linha">
-                    <input type="email" placeholder="Email:" className='campo-um-input' onChange={(e)=>setEmail(e.target.value)}/>
-                </div>
-
-                <div className="linha">
-                <input type="text" className="campo-um-input" placeholder="CPF:" onChange={(e)=> setCpf(e.target.value)}/>
-                </div>
-                <div className="linha">
-                    <input type='number' className='campo' placeholder='Número' onChange={(e)=> setNumeroEndereco(e.target.value)}/>
-                    <input type="date" className="campo" placeholder="Data de nascimento:" onChange={(e)=> setDtNascimento(e.target.value)}/>
-                </div>
-                <input type="submit" />
-                </form>
-            </div>
-            
-        </div>
     )
 }
--- client/kiarnet/src/pages/Cadastro/index.jsx
+++ client/kiarnet/src/pages/Cadastro/index.jsx
@@ -1,69 +1,215 @@
-import { createContext, useState } from 'react';
-import InformeDados from '../../components/cadastro-passos/Informedados';
+import {useState } from 'react';
 import './index.scss'
 import Barraprogresso from '../../components/Barraprogresso';
-import Pagamento from '../../components/cadastro-passos/Pagamento';
-import BancoConta from '../../components/inserir-dados-bancarios';
+import {useForm} from 'react-hook-form'
 
-//TODO: Aproveitando que a API do viacep verifica região, definir que apenas regiões sul/sudeste podem se inscrever, tela de erro
- export const contextoCadastro = createContext()
-export function Cadastro(){
+export function Cadastro() {
     const [passo, setPasso] = useState(0)
-    
-
-    const [nomeCompleto, setNomeCompleto] = useState('')
-    const [celular, setCelular] = useState('')
-    const [telefone, setTelefone ] = useState('')
-    const [email, setEmail] = useState('')
-    const [cpf, setCpf] = useState('')
-    const [dtNascimento, setDtNascimento] = useState('')
-    const [escolhaDebito, setEscolhaDebito] = useState(false)
-    const [tipoResidencia, setTipoResidencia] = useState('')
-    const [emailFatura, setEmailFatura] = useState('')
-    const [diaVencimento, setDiaVencimento] = useState(0)
-    const [cep, setCep] = useState(0)
-    const [dadosEndereco, setDadosEndereco] = useState({})
-    const [numeroEndereco, setNumeroEndereco] = useState(0)
-    function proximoPasso(){
+    const { watch, register, formState: {errors} } = useForm({mode: "all"})
+    function proximoPasso() {
 
     }
-    function passoAnterior(){
-        if(!passo >= 0){
+    function passoAnterior() {
+        if (!passo >= 0) {
             setPasso(passo - 1)
         }
     }
-    return(
-    
-    <div className="conteudo">
-        <div className="barra-progresso" style={{position: 'sticky', width: '100%', top: '10px'}}>
-            <Barraprogresso progresso={(35*passo)}/>
-            <button onClick={passoAnterior}>Voltar</button>
-        </div>
 
-        <contextoCadastro.Provider value={
-            [
-            nomeCompleto, setNomeCompleto, 
-            celular, setCelular,
-            telefone, setTelefone,
-            email, setEmail,
-            cpf, setCpf,
-            dtNascimento, setDtNascimento,
-            escolhaDebito, setEscolhaDebito,
-            tipoResidencia, setTipoResidencia,
-            emailFatura, setEmailFatura,
-            diaVencimento, setDiaVencimento,
-            cep, setCep,
-            dadosEndereco, setDadosEndereco,
-            numeroEndereco, setNumeroEndereco
-            ]
-            }>
-            <div className="campo-passos">
-                {passo === 0 && <InformeDados/>}
-                {passo === 1 && <Pagamento/>}
-                {passo === 2 && <BancoConta/>}
+    return (
+
+
+
+        <div className="conteudo">
+            <div className="barra-progresso" style={{ position: 'sticky', width: '100%', top: '10px' }}>
+                <Barraprogresso progresso={(33 * passo)} />
+                <button onClick={passoAnterior}>Voltar</button>
             </div>
-        </contextoCadastro.Provider>
-        <button className='botao' onClick={()=>setPasso(passo + 1)}>Avançar</button>
-    </div>
+
+
+            <div className="campo-passos">
+                <form onSubmit={(e)=> console.log(e)}>
+                    {passo === 0 &&
+                               <div className="conteudo">
+                               <h1>Informe seus dados</h1>
+                               <div className="campos-informedados">
+                  
+                                   <div className="linha">
+                                   <input 
+                                        type="text" 
+                                        placeholder="Nome completo" 
+                                        className='campo-um-input' 
+                                        id="username"
+                                        name="username" 
+                                        {...register('nome', {required: true, message: "O nome completo é obrigatório fera" })}
+                                        />
+                                        {errors.username && <p>errors.username</p>}
+                                   </div>
+                                   
+                                   <div className="linha">
+                                       <input 
+                                        type="text"
+                                        pattern='[0-9]+' 
+                                        placeholder="CEP:" 
+                                        className='campo-um-input' 
+                                        {...register('cep', {required: true, message: "CEP obrigatorio"})}/>
+                                   </div>
+                   
+                                   <div className="linha">
+                                       <input type="tel" className="campo" placeholder="Celular"
+                                       {...register('celular', {required: true})} />
+                   
+                                       <input type="tel" className="campo" placeholder="Telefone (opcional)"
+                                       {...register('telefone', {required: false})}  />
+                                   </div>
+                   
+                                   
+                                   <div className="linha">
+                                       <input type="email" placeholder="Email:" className='campo-um-input' 
+                                       {...register('email', {required: false})}  />
+                                   </div>
+                   
+                                   <div className="linha">
+                                   <input type="text" className="campo-um-input" placeholder="CPF:" 
+                                    {...register('cpf', {required: true})}  />
+                                   </div>
+                                   <div className="linha">
+                                       <input type='number' className='campo' placeholder='Número'
+                                       {...register('numeroEndereco', {required:true})}/>
+                                       <input type="date" className="campo" placeholder="Data de nascimento:" 
+                                       {...register('datanascimento', {required: true})} />
+                                   </div>
+                               </div>   
+                               <pre>
+                           {JSON.stringify(watch(), null, 2)}
+                           <input type="submit"/>
+                       </pre>
+
+                               <button className='botao' onClick={()=>setPasso(passo + 1)}>proximo</button>
+                           </div>
+                           
+
+                          
+
+                    }
+                    {
+                        passo === 1 &&
+                        <div className="conteudo-passo2">
+   
+                                <p>Escolha a data de vencimento:</p>
+                                <div className="opcoes-data">
+                                    <button >24</button>
+                                    <button >18</button>
+                                    <button >12</button>
+                                    <button >06</button>
+                                </div>
+
+                                <div className="campos-informedados">
+                                    <p>Informe seu email para envio da fatura:</p>
+
+                                    <div className="campo-email">
+                                        <input type="text" className="campo-um-input" placeholder="email para envio da fatura" />
+                                        <input type="text" className="campo-um-input" placeholder="Confirme o email"  />
+                                    </div>
+
+
+
+                                    <p>Deseja cadastrar sua conta em débito automatico?</p>
+                                    <div className="campo">
+
+                                        <div className="campo-input-radio">
+                                            <div className="input-radio">
+                                                <input type="radio" name='selecao-debito' id='selecao-debito-sim'></input>
+                                                <label htmlFor="selecao-debito-sim">Sim</label>
+                                            </div>
+
+                                            <div className="input-radio">
+                                                <input type="radio" name='selecao-debito' id='selecao-debito-nao'></input>
+                                                <label htmlFor="selecao-debito-nao">Não</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <p>Confirme seu endereço:</p>
+
+                                    <div className="info-endereco">
+                                        <div className="linha-info-endereco">
+                                            <p className='nome-linha'>CEP:</p>
+                                            <p></p>
+                                        </div>
+                                        <div className="linha-info-endereco">
+                                            <p>Bairro:</p>
+                                            <p></p>
+                                        </div>
+                                        <div className="linha-info-endereco">
+                                            <p className='nome-linha'>Cidade:</p>
+                                            <p></p>
+                                        </div>
+                                        <div className="linha-info-endereco">
+                                            <p className='nome-linha'>Estado:</p>
+                                            <p></p>
+                                        </div>
+                                        <div className="linha-info-endereco">
+                                            <p className='nome-linha'>Endereço/Rua:</p>
+                                            <p></p>
+                                        </div>
+                                        <div className="linha-info-endereco">
+                                            <p>Número:</p>
+                                            <p></p>
+                                        </div>
+
+                                        <div className="campo-tipo-residencia">
+                                            <p>Selecione o tipo da sua residência:</p>
+                                            <div className="campo-input-radio">
+                                                <div className="input-radio">
+                                                    <input type="radio" name="selecao-residencia" id="selecao-residencia-casa"  />
+                                                    <label htmlFor="selecao-residencia-casa">Casa</label>
+                                                </div>
+
+                                                <div className="input-radio">
+                                                    <input type="radio" name="selecao-residencia" id="selecao-residencia-apartamento"/>
+                                                    <label htmlFor="selecao-residencia-apartamento">Apartamento</label>
+                                                </div>
+                                            </div>
+
+                                            <input type="text" className='complemento' placeholder='Complemento (opcional)' maxLength="120" />
+                                        </div>
+
+                                        <button  className='botao' onClick={()=>setPasso(passo + 1)}>proximo</button>
+                                        
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                    }
+                    {
+                        passo === 2 &&
+                        <div className="conteudo-passo3">
+                            <h1>Selecione as datas de instalação</h1>
+
+                                <div className="opcoes-instalacao">
+                                    <div className="opcao-instalacao">
+                                        <p>primeira opção</p>
+                                        <input type="date" name="" id="" />
+                                    </div>
+
+                                    <div className="opcao-instalacao" style={{marginTop: "30px"}}>
+                                        <p>Segunda opção</p>
+                                        <input type="date" name="" id="" />
+                                    </div>
+                                </div>
+
+                                <button className="botao">
+                                    Finalizar cadastro
+                                </button>
+                            </div>
+
+                    }
+
+                </form>
+
+            </div>
+        </div>
     )
 }
