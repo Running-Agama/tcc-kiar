eebf1cbfc27a51d937489dec96b78b0a920d7d47
nishikawaq <vi.srnishikawa.2007@gmail.com>
2024-11-13 11:32:06 -0300

    dart

---
+++ client/dart/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {Route, BrowserRouter,Routes} from 'react-router-dom'
+import Landing from './pages/Landing';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+     
+     <BrowserRouter>
+          <Routes>
+               <Route path='/landing' element={<Landing/>}/>   
+          </Routes>
+     </BrowserRouter>
+
+);
