d9dd171263afb8ad5421517ae534171e5cd96c26
nishikawaq <vi.srnishikawa.2007@gmail.com>
2024-11-13 11:40:04 -0300

    pasta index

--- client/dart/src/index.js
+++ client/dart/src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Route, BrowserRouter,Routes} from 'react-router-dom'
 import Landing from './pages/Landing';
+import Sobre from './pages/sobre';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
      
      <BrowserRouter>
           <Routes>
-               <Route path='/landing' element={<Landing/>}/>   
+          <Route path='/' element={<Landing/>}/> 
+          <Route path='/sobre' element={<Sobre/>}/> 
           </Routes>
      </BrowserRouter>
 
---
+++ client/dart/src/pages/Landing/index.jsx
@@ -0,0 +1,10 @@
+import './index.scss'
+export default function Landing() {
+
+    return (
+       <div className="mae">
+        <h1>kk</h1>
+       </div>
+    )
+}
+
