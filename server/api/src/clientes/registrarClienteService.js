f557d50d4ee35a1a1861ef50d42313e2703951bf
GuilhermeGomes76 <guilhermegomessilva76@gmail.com>
2024-11-22 12:07:17 +0000

    tentando nao faze r merda

--- server/api/src/clientes/registrarClienteService.js
+++ server/api/src/clientes/registrarClienteService.js
@@ -2,6 +2,7 @@ import clientesRepository from "../repository/clientes/clientesRepository.js"
 import enviarEmail from "../utils/enviarEmail.js"
 
 export default async function registrarClienteService(corpo){
+    console.log('service')
     const resposta = await clientesRepository.cadastrarCliente(corpo)
     const email = await enviarEmail(corpo.email)
     console.log('passou aqui')
