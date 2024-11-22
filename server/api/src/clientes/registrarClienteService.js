0c707fd611fd69800c01da419b8b03ffd211a06f
GuilhermeGomes76 <guilhermegomessilva76@gmail.com>
2024-11-22 12:10:32 +0000

    m

--- server/api/src/clientes/registrarClienteService.js
+++ server/api/src/clientes/registrarClienteService.js
@@ -5,6 +5,7 @@ export default async function registrarClienteService(corpo){
     console.log('service')
     const resposta = await clientesRepository.cadastrarCliente(corpo)
     const email = await enviarEmail(corpo.email)
+    console.log(email)
     console.log('passou aqui')
     return resposta
 }
