4894dac21493a4124baaab6a29385e6c4fe59162
GuilhermeGomes76 <guilhermegomessilva76@gmail.com>
2024-11-22 11:36:09 +0000

    arrumando o chamar funcao

--- server/api/src/clientes/registrarClienteService.js
+++ server/api/src/clientes/registrarClienteService.js
@@ -1,7 +1,8 @@
 import clientesRepository from "../repository/clientes/clientesRepository.js"
+import enviarEmail from "../utils/enviarEmail.js"
 
 export default async function registrarClienteService(corpo){
     const resposta = await clientesRepository.cadastrarCliente(corpo)
-    envi
+    enviarEmail(corpo.email)
     return resposta
 }
